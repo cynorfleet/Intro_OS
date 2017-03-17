09d22e33dd3c6042290c2a8eac1dafb5c8495be4
ElementaryOS <cynorfleet@gmail.com>
2017-03-17 14:12:43 -0500

    Piped now working. Uses Recursive algorithm

--- assignments/shell/anotherdiver.py
+++ assignments/shell/anotherdiver.py
@@ -6,16 +6,42 @@ from os import path
 LOOP_STATUS_GO = 1
 LOOP_STATUS_STOP = 0
 
-def ToFile(args, opts, source):
-    argstop = args.index('>')
-    f = open(args[argstop+1], 'w')
+def ToFile(args, aORw, source, mode):
+    argstop = args.index(aORw)
+    argtarget = args[argstop+1]
+    f = open(argtarget, mode)
+
+    if mode == 'a':
+        print("APPEND MODE")
+    elif mode == 'w':
+        print("WRITE MODE")
+
     for line in source:
         f.write('\n'+line)
     return source
 
-def List_Dir(args, opts):
+def FromFile(args):
+    mode='r'
+    print("READ MODE")
+
+    sendback = []
+    argindex = args.index('<')
+    source = args[argindex+1]
+    print("source: {}".format(source))
+    f = open(source, 'r')
+    sendback = f.read().splitlines()
+    f.close()
+    print('LINE: {}\n'.format(sendback))
+    # for line in f:
+        # sendback.append(line)
+
+
+    return sendback
+
+def List_Dir(target, opts):
     output = []
-    dirlist = os.listdir(args[1])
+    convertsize = 1024.0
+    dirlist = os.listdir(target)
 
     if opts.isAll == False:
         for file in dirlist:
@@ -23,7 +49,7 @@ def List_Dir(args, opts):
                 del dirlist[dirlist.index(file)]
 
     if opts.isLong == True:
-        parent = path.abspath(args[1])
+        parent = path.abspath(target)
         for file in dirlist:
             listindex = dirlist.index(file)
             dirlist[listindex] = parent + file
@@ -31,17 +57,16 @@ def List_Dir(args, opts):
     if opts.isRead == True:
         for file in dirlist:
             listindex = dirlist.index(file)
-            filesize = path.getsize(file) / 1024.0
+            filesize = path.getsize(file) / convertsize
             print('{:<20}{:>30.2f} kb'.format(file, filesize))
             dirlist[listindex] = filesize
 
-    print("answer:\n".format(dirlist))
     return dirlist
+
 
 def ParseCmd(raw_cmd):
     # split the command into a list
-    print(type(raw_cmd))
-    print("String {}".format(raw_cmd))
+    print("\nString {}".format(raw_cmd))
     # Define the options/args
     parser = optparse.OptionParser('usage %prog -a<bool> list all show hidden'+\
             'files -l<bool>long listing -h<bool> human readable sizes',
@@ -58,34 +83,89 @@ def ParseCmd(raw_cmd):
 
     return parser.parse_args(raw_cmd)
 
+def ListFactory(args, options):
+
+    print('Options: {}\nArgs: {}\n'.format(options, args))
+
+    dirlist = []
+
+    if any('<' in s for s in args):
+        result = []
+        argindex = args.index('<')
+        source = args[argindex+1]
+        data = FromFile(args)
+        for line in data:
+            if line == '':
+                del data[data.index(line)]
+            else:
+                result.append(List_Dir(line, options))
+                dirlist = result
+                print("Result:\n{}".format(result))
+        return result
+
+    else:
+        if len(args) < 1:
+            args.append('.')
+
+        dirlist = List_Dir(args[0], options)
+
+        if any('>>' in s for s in args):
+            mode = 'a'
+            dirlist = ToFile(args, '>>', dirlist, mode)
+            return dirlist
+
+        elif any('>' in s for s in args):
+            mode = 'w'
+            dirlist = ToFile(args, '>', dirlist, mode)
+            return dirlist
+
+        else:
+            return dirlist
+    return
+
+def _WorkLoop(split_cmd):
+    (options, args) = ParseCmd(split_cmd)
+    result = []
+
+    if args[0] == 'ls':
+        result = ListFactory(args[1:], options)
+        print (result)
+    return result
+
 def Run_Loop(raw_cmd):
 #   make a child process
     pid = os.fork()
     if pid == 0:
-#       parse the new command
-	split_cmd = raw_cmd.split()
-	print("Run_CMD String {}".format(split_cmd))
-        (options, args) = ParseCmd(split_cmd)
-        print('Options: {}\nArgs: {}\n'.format(options, args))
+        if '|' in raw_cmd:
+            print("PIPED")
+            pipedcmd = raw_cmd.split('|')
+    #       parse the new command
+            split_cmd = pipedcmd[0].split()
+            _WorkLoop(split_cmd)
 
-#      if self is a child process
-        if args[0] == 'ls':
-#           runs the specified cmd
-            dirlist = List_Dir(args, options)
-            if any('>' in s for s in args):
-                ToFile(args, options, dirlist)
-            else:
-                print("Listing:\n{}".format(dirlist))
-#             for i in ary:
-#                 print (i)
-#         elif command.cmd == 'mkdir':
+    #           runs the specified cmd
+
+    #                 print (i)
+    #         elif command.cmd == 'mkdir':
 #             Mk_Dir(command)
-#         elif command.cmd == 'cd':
-#             Chg_Dir(command)
-#         elif command.cmd == 'pwd':
-#             Print_Dir(command)
+    #         elif command.cmd == 'cd':
+    #             Chg_Dir(command)
+    #         elif command.cmd == 'pwd':
+    #             Print_Dir(command)
 
-#   if self is parent process
+            #   if self is parent process
+            del pipedcmd[0]
+            Run_Loop(pipedcmd)
+        else:
+                if type(raw_cmd) == list:
+                    for item in raw_cmd:
+                        split_cmd = item.split()
+                        print("NOT PIPED. Contains: {}. Type: {}".format(item, type(item)))
+                        _WorkLoop(split_cmd)
+                else:
+                    split_cmd = raw_cmd.split()
+                    _WorkLoop(split_cmd)
+
     elif pid > 0:
         while True:
             wpid, status = os.waitpid(pid, 0)
@@ -104,7 +184,6 @@ def shell_loop():
 
         #   Get user command
         raw_cmd = sys.stdin.readline()
-        print("Main\nRawType: {}\nRaw Content: {}\n".format(type(raw_cmd), raw_cmd))
 
         #   Run command
         status = Run_Loop(raw_cmd)
